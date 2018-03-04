524cb4817007d2f6303f5c5309b6da5f7a9255ac
patelabhi191 <patelabhi191@gmail.com>
2018-03-04 18:55:55 -0500

    <ADD> Sticks and Ball

---
+++ Pongg/Assets/Scripts/InputController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputController : MonoBehaviour
+{
+
+    public GameObject Leftbar;
+    public GameObject Rightbar;
+
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		Leftbar.GetComponent<Rigidbody>().velocity=new Vector3(0f,0f,0f);
+		if (Input.GetKey(KeyCode.W))
+        {
+			Leftbar.GetComponent<Rigidbody>().velocity=new Vector3(0f,4.5f,0f);
+        }
+		else if (Input.GetKey(KeyCode.S))
+        {
+			Leftbar.GetComponent<Rigidbody>().velocity=new Vector3(0f,-4.5f,0f);
+        }
+
+		Rightbar.GetComponent<Rigidbody>().velocity=new Vector3(0f,0f,0f);
+		if (Input.GetKey(KeyCode.UpArrow))
+		{
+			Rightbar.GetComponent<Rigidbody>().velocity=new Vector3(0f,4.5f,0f);
+		}
+		else if (Input.GetKey(KeyCode.DownArrow))
+		{
+			Rightbar.GetComponent<Rigidbody>().velocity=new Vector3(0f,-4.5f,0f);
+		}
+
+    }
+}
