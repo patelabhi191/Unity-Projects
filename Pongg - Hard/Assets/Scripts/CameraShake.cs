0d1fa02d629bf39127ed3e8cd0c6e793f03b2a37
Abhi Patel <36008861+patelabhi191@users.noreply.github.com>
2018-04-07 01:13:47 -0400

    [Add] Camera Shake

---
+++ Pongg - Hard/Assets/Scripts/CameraShake.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraShake : MonoBehaviour {
+
+	Vector3 originalPos;
+	float shakeDuration = 0.0f;
+	float shakeAmount = 0.7f;
+	float decreaseFactor = 1.0f;
+	public Camera c;
+	public GameObject Ball;
+
+	void Start()
+	{
+		originalPos = c.transform.position;
+
+		//originalPos = c.transform.localScale;
+	}
+
+	void Update()
+	{
+		if (shakeDuration > 0)
+		{
+			c.transform.position = originalPos + Random.insideUnitSphere * shakeAmount;
+			c.orthographicSize = 4.8f;
+			shakeDuration -= Time.deltaTime * decreaseFactor;
+		}
+		else
+		{
+			shakeDuration = 0.0f;
+			c.transform.position = originalPos;
+			c.orthographicSize = 5;
+		}
+		if (Ball.transform.position.x>9.5f || Ball.transform.position.x<-9.5f)
+		{
+			shakeDuration = 0.2f;
+		}
+	}
+	public void setDuration(float duration)
+	{
+		shakeDuration = duration;
+	}
+}
