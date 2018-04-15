d8a61a230544cf3fa7a52d93dbb0d6d1da76768e
Abhi Patel <36008861+patelabhi191@users.noreply.github.com>
2018-04-15 12:20:44 -0400

    [Edit] Proper tile placement

--- Bomber Man/Assets/Scripts/playermover.cs
+++ Bomber Man/Assets/Scripts/playermover.cs
@@ -6,9 +6,13 @@ public class playermover : MonoBehaviour {
 
 	public GameObject BomberBhai;
 	public float speed;
+	public Animator anim;
 
 	// Use this for initialization
-	void Start (){}
+	void Start ()
+	{
+		anim = GetComponent<Animator>();
+	}
 
 	// Update is called once per frame
 	void Update ()
@@ -17,18 +21,26 @@ public class playermover : MonoBehaviour {
 		if (Input.GetKey(KeyCode.W)||Input.GetKey(KeyCode.UpArrow))
 		{
 			BomberBhai.GetComponent<Rigidbody2D>().velocity=new Vector3(0f,speed,0f);
+			anim.SetBool ("IsRight",false);anim.SetBool ("IsLeftt",false);anim.SetBool ("IsDown",false);
+			anim.SetBool ("IsUp",true);
 		}
 		else if (Input.GetKey(KeyCode.S)||Input.GetKey(KeyCode.DownArrow))
 		{
 			BomberBhai.GetComponent<Rigidbody2D>().velocity=new Vector3(0f,-speed,0f);
+			anim.SetBool ("IsRight",false);anim.SetBool ("IsLeftt",false);anim.SetBool ("IsUp",false);
+			anim.SetBool ("IsDown",true);
 		}
 		else if (Input.GetKey(KeyCode.D)||Input.GetKey(KeyCode.RightArrow))
 		{
 			BomberBhai.GetComponent<Rigidbody2D>().velocity=new Vector3(speed,0f,0f);
+			anim.SetBool ("IsUp",false);anim.SetBool ("IsLeftt",false);anim.SetBool ("IsDown",false);
+			anim.SetBool ("IsRight",true);
 		}
 		else if (Input.GetKey(KeyCode.A)||Input.GetKey(KeyCode.LeftArrow))
 		{
 			BomberBhai.GetComponent<Rigidbody2D>().velocity=new Vector3(-speed,0f,0f);
+			anim.SetBool ("IsRight",false);anim.SetBool ("IsUp",false);anim.SetBool ("IsDown",false);
+			anim.SetBool ("IsLeft",true);
 		}
 
 	}
