113d0993e0002ca429799166764fe8174f997cc6
patelabhi191 <36008861+patelabhi191@users.noreply.github.com>
2018-04-18 13:29:37 -0400

    [Add] Movemet Animation

--- Bomber Man/Assets/Scripts/playermover.cs
+++ Bomber Man/Assets/Scripts/playermover.cs
@@ -5,44 +5,50 @@ using UnityEngine;
 public class playermover : MonoBehaviour {
 
 	public GameObject BomberBhai;
-	public float speed;
+    private float xaxis;
+    private float yaxis;
+    public float speed;
 	public Animator anim;
 
 	// Use this for initialization
 	void Start ()
 	{
 		anim = GetComponent<Animator>();
-	}
+        anim.SetFloat("y", 0f);
+        anim.SetFloat("x", 0f);
+    }
 
 	// Update is called once per frame
 	void Update ()
 	{
-		BomberBhai.GetComponent<Rigidbody2D>().velocity=new Vector3(0f,0f,0f);
-		if (Input.GetKey(KeyCode.W)||Input.GetKey(KeyCode.UpArrow))
-		{
-			BomberBhai.GetComponent<Rigidbody2D>().velocity=new Vector3(0f,speed,0f);
-			anim.SetBool ("IsRight",false);anim.SetBool ("IsLeftt",false);anim.SetBool ("IsDown",false);
-			anim.SetBool ("IsUp",true);
-		}
-		else if (Input.GetKey(KeyCode.S)||Input.GetKey(KeyCode.DownArrow))
-		{
-			BomberBhai.GetComponent<Rigidbody2D>().velocity=new Vector3(0f,-speed,0f);
-			anim.SetBool ("IsRight",false);anim.SetBool ("IsLeftt",false);anim.SetBool ("IsUp",false);
-			anim.SetBool ("IsDown",true);
-		}
-		else if (Input.GetKey(KeyCode.D)||Input.GetKey(KeyCode.RightArrow))
-		{
-			BomberBhai.GetComponent<Rigidbody2D>().velocity=new Vector3(speed,0f,0f);
-			anim.SetBool ("IsUp",false);anim.SetBool ("IsLeftt",false);anim.SetBool ("IsDown",false);
-			anim.SetBool ("IsRight",true);
-		}
-		else if (Input.GetKey(KeyCode.A)||Input.GetKey(KeyCode.LeftArrow))
-		{
-			BomberBhai.GetComponent<Rigidbody2D>().velocity=new Vector3(-speed,0f,0f);
-			anim.SetBool ("IsRight",false);anim.SetBool ("IsUp",false);anim.SetBool ("IsDown",false);
-			anim.SetBool ("IsLeft",true);
-		}
+       xaxis = Input.GetAxis("Horizontal");
+        yaxis = Input.GetAxis("Vertical");
+        BomberBhai.GetComponent<Rigidbody2D>().velocity=new Vector3(0f,0f,0f);
+        anim.SetFloat("x", xaxis);
+        anim.SetFloat("y", yaxis);
+        Debug.Log(xaxis);
+        Debug.Log(yaxis);
+         if (Input.GetKey(KeyCode.W)||Input.GetKey(KeyCode.UpArrow))
+         {
+             BomberBhai.GetComponent<Rigidbody2D>().velocity=new Vector3(0f,speed,0f);
+             //anim.SetFloat("y", yaxis);
+         }
+         else if (Input.GetKey(KeyCode.S)||Input.GetKey(KeyCode.DownArrow))
+         {
+             BomberBhai.GetComponent<Rigidbody2D>().velocity=new Vector3(0f,-speed,0f);
+             //anim.SetFloat("y", yaxis);
+         }
+         else if (Input.GetKey(KeyCode.D)||Input.GetKey(KeyCode.RightArrow))
+         {
+             BomberBhai.GetComponent<Rigidbody2D>().velocity=new Vector3(speed,0f,0f);
+            // anim.SetFloat("x", xaxis);
+         }
+         else if (Input.GetKey(KeyCode.A)||Input.GetKey(KeyCode.LeftArrow))
+         {
+             BomberBhai.GetComponent<Rigidbody2D>().velocity=new Vector3(-speed,0f,0f);
+          //   anim.SetFloat("x", xaxis);
+         }
 
-	}
+    }
 		
 }
