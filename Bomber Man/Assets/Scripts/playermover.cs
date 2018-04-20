6e835d5e6cb38b691dcbfc48a0d3e75a143bf66f
Abhi Patel <36008861+patelabhi191@users.noreply.github.com>
2018-04-20 19:25:24 -0400

    Flames prefeb gone

--- Bomber Man/Assets/Scripts/playermover.cs
+++ Bomber Man/Assets/Scripts/playermover.cs
@@ -12,6 +12,8 @@ public class playermover : MonoBehaviour {
     public float speed;
 	public Animator anim;
 	public Animator ani;
+	public float nextFire = 0.1f;
+	private float myTime = 0.0f;
 
 	// Use this for initialization
 	void Start ()
@@ -26,10 +28,12 @@ public class playermover : MonoBehaviour {
 	{
 		xaxis = Input.GetAxis ("Horizontal");
 		yaxis = Input.GetAxis ("Vertical");
+		myTime += Time.deltaTime;
 		        
-		if (Input.GetKey (KeyCode.Space))
+		if (Input.GetKey (KeyCode.Space)&& myTime > nextFire)
 		{		
 			Blast ();
+			myTime = 0f;
 		}
 	
 		BomberBhai.GetComponent<Rigidbody2D>().velocity=new Vector3(0f,0f,0f);
@@ -56,16 +60,18 @@ public class playermover : MonoBehaviour {
 	void Blast()
 	{
 		Instantiate (Bomb, BomberBhai.transform.position, Quaternion.Euler (0, 0, 0));
+
 		Vector3 loc1 = new Vector3(BomberBhai.transform.position.x+0.8f,BomberBhai.transform.position.y,0);
 		Vector3 loc2 = new Vector3(BomberBhai.transform.position.x,BomberBhai.transform.position.y+0.8f,0);
 		Vector3 loc3 = new Vector3(BomberBhai.transform.position.x-0.8f,BomberBhai.transform.position.y,0);
 		Vector3 loc4 = new Vector3(BomberBhai.transform.position.x,BomberBhai.transform.position.y-0.8f,0);
 		Instantiate (BombF, loc2 , Quaternion.Euler (0, 0, 0));
+		DestroyImmediate (BombF, true);
 		Instantiate (BombF, loc4 , Quaternion.Euler (0, 0, 180));
 		BombF.transform.Rotate(0,0,90);
 		Instantiate (BombF, loc1 , Quaternion.Euler (0, 0, 270));
 		Instantiate (BombF, loc3 , Quaternion.Euler (0, 0, 90));
 		BombF.transform.Rotate(0,0,270);
-	}
-		
+
+	}		
 }
